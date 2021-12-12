f7be060b86b653a2960f8158f9386c039b00d4cb
Audrius Kniuras <audrius.kniuras@tesonet.com>
2021-12-12 11:06:17 +0200

    ex2.12 - countries API

---
+++ part2/ex_countries/src/App.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function CountryFormatter(props) {
+  const country = props.country;
+
+  const languages = Object.values(country.languages)
+  const flag = country["flags"]["png"]
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {languages.map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={flag} alt="" />
+    </div>
+  )
+}
+
+
+function Filter(props) {
+  const countriesToShow = props.countryResult.filter((country) =>
+    country.name.common.includes(props.countryInput)
+  );
+
+  if (props.countryResult.length === 0) {
+    return (
+      <p>Getting countries...</p>
+    )
+  }
+
+  if (countriesToShow.length < 10 && countriesToShow.length !== 1) {
+    return (
+      <div>
+        {countriesToShow.map((country) => (
+          <p key={country.name.common}>{country.name.common}</p>
+        ))}
+      </div>
+    );
+  } else if (countriesToShow.length > 1) {
+    return (
+      <div>
+        <p>Too many countries to show</p>
+      </div>
+    )
+  } else {
+    return (
+      <CountryFormatter country={countriesToShow[0]} />
+    )
+  }
+}
+
+function App() {
+  const [countryInput, setCountryInput] = useState("");
+  const [countryResult, setCountryResult] = useState([]);
+
+  const handleChangeCountry = (event) => {
+    setCountryInput(event.target.value);
+  };
+
+  useEffect(() => {
+    const eventHandler = (response) => {
+      setCountryResult(response.data);
+    };
+    axios.get("https://restcountries.com/v3.1/all").then(eventHandler);
+  }, []);
+
+  return (
+    <div>
+      Find country:{" "}
+      <input value={countryInput} onChange={handleChangeCountry} />
+      <Filter countryResult={countryResult} countryInput={countryInput} />
+    </div>
+  );
+}
+
+export default App;
