9c840fa3b129343f5d98bf730c8ea2630167a598
Audrius Kniuras <audrius.kniuras@tesonet.com>
2021-12-24 14:32:37 +0200

    blog app

---
+++ blog/index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogRouter = require("./controllers/blog")
+
+require("dotenv").config();
+
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogRouter)
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
